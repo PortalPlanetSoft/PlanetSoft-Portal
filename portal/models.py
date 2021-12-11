3990e4b45a298c1a59fbd9eeec882d9f07c22c28
Bojan Kuzmanovic <bojan.kuzmanovic@student.etf.unibl.org>
2021-12-11 01:30:47 +0100

    Added models Employee and CompanyPosition to app portal, added Bootstrap 5 to templates and made basic nav bar

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class CompanyPosition(models.Model):
+    department_name = models.CharField(max_length=100)
+    position_name = models.CharField(max_length=100)
+    office_number = models.IntegerField()
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    # working position in the company
+    position = models.ForeignKey(CompanyPosition, on_delete=models.CASCADE)
+    # time joined the company
+    time_joined = models.DateField(auto_now_add=True)
