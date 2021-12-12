0beec13e2800267ebdab98d9d582ca6fa3f307b8
Ognjen <ogivuja@gmail.com>
2021-12-12 21:20:43 +0100

    created models and imported initial data

--- portal/index/models.py
+++ portal/index/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Sector(models.Model):
+    sector_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sector_name
+
+class Position(models.Model):
+    position_name = models.CharField(max_length=50)
+    sector_name = models.ForeignKey(Sector, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.position_name
+
+class Employee(models.Model):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return (f" {self.first_name} {self.last_name}")
+
