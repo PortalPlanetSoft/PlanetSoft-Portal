06f662e9c6b64d7318278f23e481e15353c7e71a
djurdjevicluka <djurdjevic.luka@gmail.com>
2021-12-18 14:08:56 +0100

    modified nav bar (removed extra buttons), removed unnecessary js slideshow, changed all elements that were aligned using tables - to divs, changed css files (where it was needed)

--- djangoProject/static/js/script.js
+++ djangoProject/static/js/script.js
@@ -1,55 +1,5 @@
-$(document).ready(function () {
-    $('li').on('click', function () {
-        let gdje = $(this).attr('rel');
-        if (gdje == 'infotable') {
-            let hash = '#';
-            let result = hash.concat(gdje);
-            $('html, body').animate({
-                scrollTop: $(result).offset().top - 130,
-                behavior: "smooth"
-            }, 200);
-        } else if (gdje == 'contact') {
-            let hash = '#';
-            let result = hash.concat(gdje);
-            $('html, body').animate({
-                scrollTop: $(result).offset().top + 70,
-                behavior: "smooth"
-            }, 200);
-        } else {
-            $('html, body').animate({
-                scrollTop: 0,
-                behavior: "smooth"
-            }, 200);
-        }
-    });
-    $('ul.nav.navbar-nav > li')
-        .click(function (e) {
-            $('ul.nav.navbar-nav > li')
-                .removeClass('active');
-            $(this).addClass('active');
-        });
-});
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000);
-}
-
 var primary = document.getElementById("maintitle");
-var secondary = document.getElementById("logomaintitle");
+
 primary.addEventListener("mouseover", hideshow);
 primary.addEventListener("mouseout", showhide);
 
