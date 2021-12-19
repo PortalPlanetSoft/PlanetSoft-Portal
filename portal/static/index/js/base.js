e0189eedcd7ec038347022d9987905551ab6afcc
Ognjen <ogivuja@gmail.com>
2021-12-19 22:40:35 +0100

    implemented functionality for dark mode button

--- portal/static/index/js/base.js
+++ portal/static/index/js/base.js
@@ -1,9 +1,17 @@
+// Navigation
 const navToggleBtn = document.querySelector('#nav-toggle-btn');
 const navigation = document.querySelector('#navigation');
+
+// Dark Mode
+const darkToggleBtn = document.querySelector('#dark-toggle-btn');
 
 navToggleBtn.addEventListener('click', (e) => {
   e.preventDefault();
   toggleNav(navigation);
+});
+darkToggleBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  toggleDark();
 });
 
 function toggleNav(navigation) {
@@ -16,4 +24,14 @@ function toggleNav(navigation) {
     return;
   }
   navigation.style.animationName = 'hideNav';
+}
+
+function toggleDark() {
+  document.documentElement.classList.toggle('dark');
+
+  darkToggleBtn.classList.toggle('btn--toggle-right');
+
+  darkToggleBtn.firstChild.className === 'fas fa-sun'
+    ? (darkToggleBtn.firstChild.className = 'fas fa-moon')
+    : (darkToggleBtn.firstChild.className = 'fas fa-sun');
 }
