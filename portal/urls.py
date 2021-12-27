4fa411e3981cda02cf9d016e685a9007b56ed69e
dvukadinovic <dvukadinovic@planetsoft.ba>
2021-12-27 13:10:23 +0100

    'Baseline Portal app'

--- portal/urls.py
+++ portal/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
     path("", views.homepage, name='homepage'),
     path("workers/", views.workersList.as_view(), name='workers'),
-    path("add/", views.addEmployee.as_view(), name='add'),
-    path("edit/<int:pk>", views.editEmployee.as_view(), name='add'),
-
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from portal.models import Employee
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
-from .forms import AddEmployee
-from django_filters.views import FilterView
-from .filtersets import EmployeeFilter
-from django.http import HttpResponse
 
 
 # Create your views here.
@@ -17,21 +12,7 @@ def homepage(request):
     return render(request, 'portal/homepage.html')
 
 
-class workersList(FilterView):
+class workersList(ListView):
     template_name = "portal/workersList.html"
     model = Employee
-    filterset_class = EmployeeFilter
 
-
-class addEmployee(CreateView):
-    model = Employee
-    form_class = AddEmployee
-    template_name = "portal/add.html"
-    success_url = '/workers/'
-
-
-class editEmployee(UpdateView):
-    model = Employee
-    form_class = AddEmployee
-    template_name = "portal/add.html"
-    success_url = '/workers/'
