2c8bd480194bc476466ab4563d9d8a058437f739
Bojan Kuzmanovic <bojan.kuzmanovic@student.etf.unibl.org>
2022-02-07 14:02:17 +0100

    Changed form and added initial migrations

--- portal/forms.py
+++ portal/forms.py
@@ -5,6 +5,10 @@ from .models import User
 
 
 class AddEditUserForm(forms.ModelForm):
+    CHOICES = [('1', 'Female'), ('0', 'Male')]
+    gender = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
+    field_order = ['username', 'password', 'first_name', 'last_name', 'email', 'gender', 'company_position']
+
     class Meta:
         model = User
         fields = {'username', 'password', 'first_name', 'last_name', 'email', 'gender', 'company_position'}
@@ -12,10 +16,16 @@ class AddEditUserForm(forms.ModelForm):
         # stavis umjesto css-klasa naziv svoje css klase i izmjenice izgled ovih polja
         widgets = {
             'username': forms.TextInput(attrs={'class': 'css-klasa'}),
-            'password': forms.TextInput(attrs={'class': 'css-klasa'}),
+            'password': forms.PasswordInput(attrs={'class': 'css-klasa'}),
             'first_name': forms.TextInput(attrs={'class': 'css-klasa'}),
             'last_name': forms.TextInput(attrs={'class': 'css-klasa1'}),
             'email': forms.TextInput(attrs={'class': 'css-klasa'}),
-            'gender': forms.CheckboxInput(attrs={'class': 'css-klasa'}),
             'company_position': forms.Select(attrs={'class': 'css-klasa'}, choices=(CompanyPosition.objects.all()))
         }
+
+    def save(self, commit=True):
+        user = super(AddEditUserForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
--- portal/views.py
+++ portal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
 from django.views.generic import TemplateView, DetailView, CreateView, UpdateView
 
 from portal.forms import AddEditUserForm
@@ -36,6 +36,6 @@ class UserUpdateView(UpdateView):
 def user_delete(request, pk):
     user = get_object_or_404(User, pk=pk)
     user.is_active = False
-    user.save()
-    # user.delete()
-    return render(request, 'portal/templates/user/users.html', {'users': User.objects.filter(is_active=True)})
+    #user.save()
+    user.delete()
+    return redirect('users')
