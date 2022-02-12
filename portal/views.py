d61a4109e4c7dd5d5ff2f28de65ed996a36b66f8
Bojan Kuzmanovic <bojan.kuzmanovic@student.etf.unibl.org>
2022-02-12 14:05:30 +0100

    Fixed pagination css, added newer fontawesome icon pack and integrated urls for create, update and delete of user

--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,4 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.db.models import Q
 from django.views.generic import TemplateView, DetailView, CreateView, UpdateView, ListView, DeleteView
 from portal.forms import AddEmployeeForm, EditEmployeeForm
 from users.models import User
@@ -51,11 +50,3 @@ class EmployeeDelete(DeleteView):
     model = User
     success_url = '/employees/'
     template_name = 'portal/templates/employee/employee-confirm-deletion.html'
-
-
-def EmployeesList(request):
-    employee_list = User.objects.all()
-    paginator = Paginator(employee_list, 9)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'portal/templates/employee/employees.html', {'page_obj': page_obj})
