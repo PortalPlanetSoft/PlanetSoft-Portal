6ee2cc5a63855deb25aa2c6d6a9188a603430046
Ognjen <ogivuja@gmail.com>
2022-02-13 20:34:41 +0100

    created javascript functionality for showing and hiding modal

--- static/portal/js/base.js
+++ static/portal/js/base.js
@@ -1,6 +1,22 @@
 const navToggleBtn = document.querySelector('#ov-nav-toggle');
 const navigation = document.querySelector('#ov-navigation');
 
+const modal = document.querySelector('#ov-modal');
+const btnshowModal = document.querySelector('#ov-btnShowModal');
+const btncloseModal = document.querySelector('#ov-btnCloseModal');
+
+btnshowModal.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.style.display = "block";
+});
+
+btncloseModal.addEventListener('click', e => {
+  e.preventDefault();
+  modal.style.display = "none";
+})
+
+
+// Navigation hamburger menu
 navToggleBtn.addEventListener('click', (e) => {
   navToggleBtn.firstChild.getAttribute('data-icon') === 'bars'
     ? navToggleBtn.firstChild.setAttribute('data-icon', 'xmark')
