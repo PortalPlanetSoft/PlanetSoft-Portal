6501a200211dd61c8b74ccc3871b0e3ff876f6ab
Bojan Kuzmanovic <bojan.kuzmanovic@student.etf.unibl.org>
2022-02-14 14:43:25 +0100

    Merged ov-frontend and ldj-psp-modal

--- portal/urls.py
+++ portal/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
 from . import views
-from .views import EmployeeDetail, EmployeeCreate, EmployeeUpdate, EmployeeList, EmployeeDelete
+from .views import EmployeeCreate, EmployeeUpdate, EmployeeList, EmployeeDelete
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='index'),
     path('employees/', EmployeeList.as_view(), name='employees'),
     path('employees/create/', EmployeeCreate.as_view(), name='employee-create'),
-    #path('employees/<int:pk>/', EmployeeDetail.as_view(), name='employee-detail'),
     path('employees/<int:pk>/', EmployeeUpdate.as_view(), name='employee-edit'),
-    path('employees/delete/<int:pk>/', EmployeeDelete.as_view(), name='employee-delete'),
+    path('employees/<int:pk>/', EmployeeDelete.as_view(), name='employee-delete'),
 ]
