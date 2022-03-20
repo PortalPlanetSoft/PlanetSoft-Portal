fe95d2355f4b79e83abf1e1664ff3cb9baabd8bd
Bojan Kuzmanovic <bojan.kuzmanovic@student.etf.unibl.org>
2022-03-20 23:10:05 +0100

    Added basic calendar view and defined urls

---
+++ events/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.urls import path
+
+from events.views import EventCreate, EventUpdate, EventDelete, DayDetails, CalendarView
+
+urlpatterns = [
+    path('<int:year>/<int:month>', login_required(CalendarView.as_view()), name='calendar'),
+    path('create/', login_required(EventCreate.as_view()), name='event-create'),
+    path('<int:pk>/', login_required(EventUpdate.as_view()), name='event-edit'),
+    path('delete/<int:pk>/', login_required(EventDelete.as_view()), name='event-delete'),
+    path('events/<int:year>/<int:month>/<int:day>', login_required(DayDetails.as_view()), name='day-events'),
+]
