b7f8cc31c78e5a1405e1bc0016fda092febf2ba6
Ognjen <ogivuja@gmail.com>
2022-03-24 17:56:31 +0100

    implemented responsive comments in javascript as well

--- static/users/js/base.js
+++ static/users/js/base.js
@@ -3,13 +3,24 @@ const navigation = document.querySelector('#ov-navigation');
 const newsCommentInput = document.querySelector("#news-comment-input");
 const commentIcon = document.querySelector("#comment-icon");
 
+console.log(window.innerWidth);
+
 if(newsCommentInput!== undefined) {
     newsCommentInput.addEventListener('input', () => {
-        newsCommentInput.value.length > 0 ?
-        commentIcon.style.visibility = "visible": 
-        commentIcon.style.visibility = "hidden"; 
-        newsCommentInput.style.height = "16px";
-        newsCommentInput.style.height = (newsCommentInput.scrollHeight) + "px";
+        if(newsCommentInput.value.length > 0) {
+            commentIcon.style.visibility = "visible";
+            window.innerWidth > 1200 ?
+            newsCommentInput.style.height = "16px":
+            newsCommentInput.style.height = "40px"
+            newsCommentInput.style.height = (newsCommentInput.scrollHeight) + "px";
+        }
+        else {
+            window.innerWidth > 1200 ?
+            newsCommentInput.style.height = "16px":
+            newsCommentInput.style.height = "40px"
+            commentIcon.style.visibility = "hidden"; 
+        }
+
     })
 }
 
