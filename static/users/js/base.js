a4820fe9d0218aa4f45177c9e6966a5b6f4d3d0a
Ognjen <ogivuja@gmail.com>
2022-03-26 16:02:33 +0100

    refactored js code for news to a seperate file

--- static/users/js/base.js
+++ static/users/js/base.js
@@ -2,29 +2,6 @@
 
 const navToggleBtn = document.querySelector('#ov-nav-toggle');
 const navigation = document.querySelector('#ov-navigation');
-const newsCommentInput = document.querySelector("#news-comment-input");
-const commentIcon = document.querySelector("#comment-icon");
-
-console.log(window.innerWidth);
-
-if(newsCommentInput!== undefined) {
-    newsCommentInput.addEventListener('input', () => {
-        if(newsCommentInput.value.length > 0) {
-            commentIcon.style.visibility = "visible";
-            window.innerWidth > 1200 ?
-            newsCommentInput.style.height = "16px":
-            newsCommentInput.style.height = "40px"
-            newsCommentInput.style.height = (newsCommentInput.scrollHeight) + "px";
-        }
-        else {
-            window.innerWidth > 1200 ?
-            newsCommentInput.style.height = "16px":
-            newsCommentInput.style.height = "40px"
-            commentIcon.style.visibility = "hidden"; 
-        }
-
-    })
-}
 
 // pageOnLoad function that fetches action result from sessionStorage so the toast alert can be shown
 $(function () {
