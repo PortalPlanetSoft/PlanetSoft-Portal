4596d54cfb2f7bdac59c91f94ded171146d708a3
Dejan <dejanforbusiness@gmail.com>
2022-04-18 12:58:55 +0200

    Brisanje console.log

--- static/js/users/profile.js
+++ static/js/users/profile.js
@@ -1,6 +1,5 @@
 
 addImagePreview("id_profile_pic", "photo-preview" );
- console.log("Tu smo negde");
 
 function addImagePreview(imageFeild_id, imagePreview_id) {
     document.getElementById(`${imageFeild_id}`).addEventListener("change", function (e) {
