8ba94e411d23bca48867be5e9e730f93297af6e8
Bojan Kuzmanovic <bojan.kuzmanovic@student.etf.unibl.org>
2022-04-23 00:08:19 +0200

    Updated models and fixed bug

--- dms/models.py
+++ dms/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 class Folder(models.Model):
     parent = models.ForeignKey('Folder', null=True, default=None, on_delete=models.CASCADE)
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, null=False, blank=False)
 
 
 class File(models.Model):
     parent = models.ForeignKey(Folder, null=True, default=None, on_delete=models.CASCADE)
-    name = models.CharField(max_length=256)
-    extension = models.CharField(max_length=3)
+    name = models.CharField(max_length=256, null=False, blank=False)
+    extension = models.CharField(max_length=10)
+    file = models.FileField(upload_to='files')
--- dms/views.py
+++ dms/views.py
@@ -1,4 +1,5 @@
-from django.http import JsonResponse
+from django.core import serializers
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 from dms.models import Folder, File
@@ -8,10 +9,8 @@ class RootFolder(TemplateView):
     template_name = 'dms/dms.html'
 
 
-
-
 def get_files(self, name):
-    return JsonResponse({
-        'folders': list(Folder.objects.filter(folder__name=name)),
-        'files': list(File.objects.filter(folder__name=name))
-    })
+    data = list(File.objects.filter(parent__name=name))
+    data.extend(list(Folder.objects.filter(parent__name=name)))
+    data = serializers.serialize('json', data)
+    return HttpResponse(data, content_type='application/json')
